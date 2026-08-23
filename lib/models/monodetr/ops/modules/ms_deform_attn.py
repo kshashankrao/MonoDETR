e3a935d0b503d1f4352e1a73773ad4b48dfe4a56
Shashank Rao Kadapanatham <shashank.kadapanatham@gmail.com>
2026-08-23 22:52:30 +0200

    Fix buggy PyTorch version check for torch.overrides

--- lib/models/monodetr/ops/modules/ms_deform_attn.py
+++ lib/models/monodetr/ops/modules/ms_deform_attn.py
@@ -52,10 +52,10 @@ from torch.nn.modules.utils import _single, _pair, _triple, _list_with_default
 from torch.nn import grad
 from torch import _VF
 from torch._jit_internal import boolean_dispatch, List, Optional, _overload, Tuple
-if float(torch.__version__.split('.')[0]) == 0 or (float(torch.__version__.split('.')[0]) == 1 and float(torch.__version__.split('.')[1])) < 7:
-    from torch._overrides import has_torch_function, handle_torch_function
-else:
+try:
     from torch.overrides import has_torch_function, handle_torch_function
+except ImportError:
+    from torch._overrides import has_torch_function, handle_torch_function
 Tensor = torch.Tensor
 
 from torch.nn.functional import linear, pad, softmax, dropout
